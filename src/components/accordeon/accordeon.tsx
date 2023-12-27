45c894ec2926ef56f91e11eb25770eac2d72a4f8
Eddy Eyme-Rossat <eddy.eymepro@gmail.com>
2023-12-27 10:06:16 +0100

    progress on accordeon

--- src/components/accordeon/accordeon.tsx
+++ src/components/accordeon/accordeon.tsx
@@ -18,55 +18,105 @@ export const Accordeon = component$<AccordeonProps>(({ style }) => {
   const $track = useSignal<HTMLDivElement>();
   const $accordeon = useSignal<HTMLDivElement>();
 
-  const computeCenterElement = $(() => {
+  const moveTrack = $((perc: number) => {
+    $track.value!.style.transform = `translate(${33 + perc}%,0%)`;
+  });
+
+  // const computeCenterElement = $((andCenter = false) => {
+  //   const trackRect = $accordeon.value!.getBoundingClientRect();
+  //   const center = trackRect.left + trackRect.width / 2;
+  //   const trackChildren = Array.from($track.value!.children);
+
+  //   trackChildren.forEach((element) => {
+  //     const elem = element as HTMLElement;
+  //     const rect = elem.getBoundingClientRect();
+  //     const elemX = rect.left;
+  //     const elemWidth = rect.width;
+
+  //     elem.style.transform = "";
+  //     if (elemX < center && center < elemX + elemWidth) {
+  //       elem.style.transform = `scale3d(1.12,1.12,1.12)`;
+
+  //       if (andCenter) {
+  //         // move the element to the center
+  //         // compute center to center distance
+  //         const elemCenter = elemX + elemWidth / 2;
+  //         const deltaCenter = center - elemCenter;
+  //         let nxt = percentage.value;
+  //         const dltCtrPerc = (deltaCenter / trackRect.width) * 100;
+  //         console.log(dltCtrPerc);
+  //         nxt += dltCtrPerc;
+  //         $track.value!.style.transition = `transition: transform 1000ms ease-in-out`;
+  //         setTimeout(() => {
+  //           moveTrack(nxt);
+  //         }, 20);
+  //       }
+  //     }
+  //   });
+  // });
+
+  const computeClosestElement = $((andCenter = false) => {
     const trackRect = $accordeon.value!.getBoundingClientRect();
     const center = trackRect.left + trackRect.width / 2;
     const trackChildren = Array.from($track.value!.children);
-
+    let closest: Element | undefined = undefined;
+    let lastDelta = -1;
     trackChildren.forEach((element) => {
       const elem = element as HTMLElement;
       const rect = elem.getBoundingClientRect();
       const elemX = rect.left;
       const elemWidth = rect.width;
+      const elemCenter = elemX + elemWidth / 2;
 
       elem.style.transform = "";
-      if (elemX < center && center < elemX + elemWidth) {
-        elem.style.transform = `scale3d(1.12,1.12,1.12)`;
+      if (Math.abs(center - elemCenter) < lastDelta || lastDelta == -1) {
+        lastDelta = Math.abs(center - elemCenter);
+        closest = element;
       }
     });
+
+    if (!closest) return;
+    const elem = closest as HTMLElement;
+    elem.style.transform = `scale3d(1.12,1.12,1.12)`;
+    const rect = elem.getBoundingClientRect();
+    const elemX = rect.left;
+    const elemWidth = rect.width;
+    if (andCenter) {
+      // move the element to the center
+      // compute center to center distance
+      const elemCenter = elemX + elemWidth / 2;
+      const deltaCenter = center - elemCenter;
+      let nxt = percentage.value;
+      const dltCtrPerc = (deltaCenter / trackRect.width) * 100;
+      console.log(dltCtrPerc);
+      nxt += dltCtrPerc;
+      percentage.value = nxt
+      moveTrack(nxt)
+    }
   });
-
-  const moveTrack = $((perc: number) => {
-    $track.value!.style.transform = `translate(${33 + perc}%,0%)`;
-    computeCenterElement();
-  });
-
-
 
   useVisibleTask$(() => {
-    computeCenterElement();
+    computeClosestElement(true);
   });
 
   return (
     <div
-       onwheel$={(e: WheelEvent) => {
-        
-        
+      onwheel$={(e: WheelEvent) => {
         let nxt = percentage.value;
         if (e.deltaY > 0) {
-          nxt -= .75;
-        } else {
+          nxt -= 0.75;
+        } else if (e.deltaY < 0) {
           nxt += 0.75;
-        } 
-        if(nxt >= 33 || nxt <= -100){
-          
-          return
-        }else {
-          e.preventDefault()
+        }
+        if (nxt >= 33 || nxt <= -100) {
+          return;
+        } else {
+          e.preventDefault();
         }
         percentage.value = nxt;
-        
+
         moveTrack(nxt);
+        computeClosestElement(false);
       }}
       class={["accordeon"]}
       ref={$accordeon}
